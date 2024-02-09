df7a1dbf345660bf0813f400659304704bf96062
Drakonz <darkprince.selvan@gmail.com>
2024-02-09 22:11:11 +0530

    updating routes and adding db

--- index.js
+++ index.js
@@ -1,57 +1,35 @@
+require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+const Character = require('./models/character');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('dist'));
-let data = [
-  {
-    id: 1,
-    content: 'Leon-kennedy',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Chris Redfield',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'Claire Redfield',
-    important: true,
-  },
-];
 
 app.get('/', (req, res) => res.send('api working!'));
 
 app.get('/api/character', (req, res) => {
-  res.send(data);
+  Character.find({}).then((char) => {
+    res.json(char);
+  });
 });
 
-app.get('/api/character/:id', (req, res) => {
+app.get('/api/character/:id', (req, res, next) => {
   const id = req.params.id;
-  const newInfo = data.find((person) => person.id === +id);
-  if (newInfo) {
-    res.send(newInfo);
-  } else {
-    res.status(404).end();
-  }
+  Character.findById(id)
+    .then((char) => {
+      if (char) {
+        res.json(char);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next.error);
 });
 
-app.delete('/api/character/:id', (req, res) => {
-  const id = req.params.id;
-  data = data.filter((person) => person.id !== id);
-  res.status(204).end();
-});
-
-const generateId = () => {
-  const maxId =
-    character.length > 0 ? Math.max(...character.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
-app.post('/api/character', (req, res) => {
+app.post('/api/character', (req, res, next) => {
   const body = req.body;
 
   if (!body.content) {
@@ -60,21 +38,59 @@ app.post('/api/character', (req, res) => {
     });
   }
 
-  const newData = {
+  const character = new Character({
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
+    important: body.important || false,
+  });
+
+  character
+    .save()
+    .then((saveChar) => {
+      res.json(saveChar);
+    })
+    .catch((error) => next(error));
+});
+
+app.put('/api/characters/:id', (req, res, next) => {
+  const body = req.body;
+
+  const char = {
+    content: body.content,
+    important: body.important,
   };
 
-  data = data.concat(newData);
+  Character.findByIdAndUpdate(req.params.id, char, { new: true })
+    .then((updateChar) => {
+      res.json(updateChar);
+    })
+    .catch((error) => next(error));
+});
 
-  res.json(note);
+app.delete('/api/character/:id', (req, res, next) => {
+  Character.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndPoint = (req, res) => {
   res.status(404).send({ error: 'unknow endpoint' });
 };
 app.use(unknownEndPoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, console.log('connected to port 3001'));
---
+++ models/character.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+const url = process.env.MONGODB_URI;
+
+console.log('connecing to', url);
+
+mongoose
+  .connect(url)
+  .then((result) => console.log('connnected to mongoDB'))
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+const characterSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 6,
+    required: true,
+  },
+  important: Boolean,
+});
+
+characterSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Character', characterSchema);
